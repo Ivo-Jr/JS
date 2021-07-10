b4aed00daef9059c7323569c4beaffd7b73625b0
IvoJr <maciel.jr95@gmail.com>
2021-07-10 20:23:54 -0300

    update

--- JS/scripts.js
+++ JS/scripts.js
@@ -136,3 +136,32 @@ x = 3 */
 
  /* console.log( 3 > 2 && 2 > 1) */
   // true. 
+
+
+  // SWITCH CASE (calculadora)
+
+  function calculate(number1, operator, number2) {
+    let result = 0;
+
+    switch (operator) {
+      case '+':
+        result = number1 + number2;
+        break
+      case '-':
+        result = number1 - number2;
+        break
+      case '*':
+        result = number1 * number2;
+        break
+      case '/':
+        result = number1 / number2;
+        break
+        default:
+      console.log('não implementado')
+      break
+    }
+
+    return result
+  }
+
+  console.log(calculate(2,'*', 9))
