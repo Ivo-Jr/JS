c46456ab07f3febb5a0fd45d3d66d2d452c3cca3
IvoJr <maciel.jr95@gmail.com>
2021-07-22 23:29:24 -0300

    exercise 'searchingAndFinding' finished

--- JS/exercise/searchingAndFinding.js
+++ JS/exercise/searchingAndFinding.js
@@ -4,7 +4,7 @@
     * Contar o número de categorias e o número de livros em cada categoria
     * Contar o número de autores
     * Mostrar livros do autor Augusto Cury
-    * Transferir a função acime em um função que irá receber o nome do autor 
+    * Transferir a função acima em um função que irá receber o nome do autor 
     e devolver os livros desse autor;
 */
 
@@ -44,3 +44,61 @@ const booksByCategory = [
     ],
   },
 ]
+
+let categoryAmount = booksByCategory.length
+let booksAmount;
+
+/* console.log(`Total de categorias, ${categoryAmount}`)
+
+booksByCategory.map(item => {
+  console.log(`Total de livros em ${item.category}, ${item.books.length}`)
+}) */
+
+// let books = booksByCategory.map(item => console.log(item.books[0].author));
+
+// 2:
+
+// aparentemente para percorrer cada array, você utiliza um "map";
+// map é similar ao "for (of)";
+
+let numbersOfAuthors = [];
+let authorBooks = [];
+
+// function AuthorCount() {
+//   booksByCategory.map(item => {
+//     item.books.map(item => {
+//       if (numbersOfAuthors.indexOf(item.author) === -1) {
+//         numbersOfAuthors.push(item.author)
+//       }
+//     });
+//   });
+//   console.log(numbersOfAuthors.length);
+// }
+
+AuthorCount();
+
+// function authorBook(author) {
+//   booksByCategory.map(item => {
+//     item.books.map(item => {
+//       if(item.author === author) {
+//         authorBooks.push(item.title)
+//       }
+//     })
+//   })
+// }
+
+// authorBook('George S. Clason')
+// console.log(authorBooks)
+
+
+// console.log(books)
+// console.log(books)
+
+// books.map(item => console.log(item[1]));
+
+// for(let category of booksByCategory) {
+//   for(let book of category.books) {
+//     console.log(book.author)
+//   }
+// }
+ 
